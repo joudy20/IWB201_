2ae4b0fe2116d7d0de0f30633f2598bca5ee05e5
joudy20 <124151258+joudy20@users.noreply.github.com>
2025-01-13 18:51:24 +0300

    Add files via upload

---
+++ home.vb
@@ -0,0 +1,47 @@
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Home</title>
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+    <header>
+
+        <h1>Welcome to the Library Website</h1>
+
+    </header>
+
+    <main>
+
+        <section>
+
+            <h2>Group Members</h2>
+
+            <ul>
+
+                <li>وائل خالد الريس (waeel khaled alrayes) - 237268</li>
+
+                <li>جودي وائل أسعد (joudy wael asaad) - 235948</li>
+
+                <li> حنان احمد الجاجه(Hanan Ahmad jajah)-241787</li>
+
+                 <li> ثائر ملهم بوظان ( Thaeer molham bouzan )-259471</li>
+
+            </ul>
+
+        </section>
+
+    </main>
+
+</body>
+
+</html>
